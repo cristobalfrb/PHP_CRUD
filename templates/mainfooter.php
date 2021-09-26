aa220f6446980ca629c1b323f8887814f6ade7fc
Cristobal Ruiz <cristobalfruizb23@gmail.com>
2021-09-26 16:26:39 -0300

    Orden del template

---
+++ templates/mainfooter.php
@@ -0,0 +1,20 @@
+    <!-- CONTENIDO -->
+
+    <!-- Scripts JS -->
+    <script src="../../public/js/jquery.js"></script>
+    <script src="../../public/js/datatables.min.js"></script>
+    <script src="../../public/js/datatables.bootstrap5.min.js"></script>
+    <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.9.3/dist/umd/popper.min.js" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.1/dist/js/bootstrap.min.js" integrity="sha384-skAcpIdS7UcVUC05LJ9Dxay8AXcDYfBJqt1CJ85S/CFujBsIzCIv+l9liuYLaMQ/" crossorigin="anonymous"></script>
+
+    <!-- LIBRERIAS PARA BOTONES DE EXPORT -->
+
+    <script src="../../public/js/dataTables.buttons.min.js"></script>
+    <script src="../../public/js/jszip.min.js"></script>
+    <script src="../../public/js/pdfmake.min.js"></script>
+    <script src="../../public/js/vfs_fonts.js"></script>
+    <script src="../../public/js/buttons.html5.min.js"></script>
+
+    <script src="https://kit.fontawesome.com/7225277ba0.js" crossorigin="anonymous"></script>
+
+    <script src="//cdn.jsdelivr.net/npm/sweetalert2@11"></script>
--- views/MntProducto/index.php
+++ views/MntProducto/index.php
@@ -1,14 +1,11 @@
 <!DOCTYPE html>
-<html lang="en">
+<html lang="es">
 
 <head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-F3w7mX95PdgyTmZZMECAngseQB83DfGTowi0iMjiWaeVhAn4FJkqJByhZMI3AhiU" crossorigin="anonymous">
-
-    <link rel="stylesheet" href="../../public/css/datatables.bootstrap5.min.css">
-    <link rel="stylesheet" href="../../public/css/buttons.dataTables.min.css">
+    <?php
+        require_once('../../templates/mainheader.php');
+    ?>
+    
     <title>Crud - Productos</title>
 </head>
 
@@ -16,24 +13,9 @@
 
     <!-- NAVBAR -->
 
-    <nav class="navbar navbar-expand-lg navbar-light bg-light">
-        <div class="container-fluid">
-            <a class="navbar-brand" href="#">Productos</a>
-            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
-                <span class="navbar-toggler-icon"></span>
-            </button>
-            <div class="collapse navbar-collapse" id="navbarNav">
-                <ul class="navbar-nav">
-                    <li class="nav-item">
-                        <a class="nav-link" aria-current="page" href="#">Inicio</a>
-                    </li>
-                    <li class="nav-item">
-                        <a class="nav-link active" aria-current="page" href="#">Productos</a>
-                    </li>
-                </ul>
-            </div>
-        </div>
-    </nav>
+    <?php
+        require_once('../../templates/mainnav.php');
+    ?>
 
     <!-- NAVBAR -->
 
@@ -72,27 +54,9 @@
     </div>
 
 
-
-    <!-- CONTENIDO -->
-
-    <!-- Scripts JS -->
-    <script src="../../public/js/jquery.js"></script>
-    <script src="../../public/js/datatables.min.js"></script>
-    <script src="../../public/js/datatables.bootstrap5.min.js"></script>
-    <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.9.3/dist/umd/popper.min.js" crossorigin="anonymous"></script>
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.1/dist/js/bootstrap.min.js" integrity="sha384-skAcpIdS7UcVUC05LJ9Dxay8AXcDYfBJqt1CJ85S/CFujBsIzCIv+l9liuYLaMQ/" crossorigin="anonymous"></script>
-
-    <!-- LIBRERIAS PARA BOTONES DE EXPORT -->
-
-    <script src="../../public/js/dataTables.buttons.min.js"></script>
-    <script src="../../public/js/jszip.min.js"></script>
-    <script src="../../public/js/pdfmake.min.js"></script>
-    <script src="../../public/js/vfs_fonts.js"></script>
-    <script src="../../public/js/buttons.html5.min.js"></script>
-
-    <script src="https://kit.fontawesome.com/7225277ba0.js" crossorigin="anonymous"></script>
-    
-    <script src="//cdn.jsdelivr.net/npm/sweetalert2@11"></script>
+    <?php
+        require_once('../../templates/mainfooter.php');
+    ?>
     
     <script src="./mntproducto.js"></script>
 
